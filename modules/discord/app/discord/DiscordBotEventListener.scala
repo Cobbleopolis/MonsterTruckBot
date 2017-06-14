c5810cd0dfca91ef5829924fe9fc9cc53be5e683
Cobbleopolis <cobbleopolis@gmail.com>
2017-06-13 23:12:34 -0400

    Added the server owner and mods not being filtered

--- modules/discord/app/discord/DiscordBotEventListener.scala
+++ modules/discord/app/discord/DiscordBotEventListener.scala
@@ -2,7 +2,7 @@ package discord
 
 import javax.inject.{Inject, Provider}
 
-import com.cobble.bot.common.models.FilterSettings
+import com.cobble.bot.common.models.{CoreSettings, FilterSettings}
 import discord.api.DiscordCommand
 import discord.event.CommandExecutionEvent
 import discord.filters.DiscordCapsFilter
@@ -12,6 +12,8 @@ import play.api.i18n.MessagesApi
 import sx.blah.discord.api.events.EventSubscriber
 import sx.blah.discord.handle.impl.events.{MessageReceivedEvent, ReadyEvent}
 import sx.blah.discord.handle.obj.{IMessage, Status}
+
+import collection.JavaConverters._
 
 class DiscordBotEventListener @Inject()(implicit configuration: Configuration, discordBot: Provider[DiscordBot], messages: MessagesApi, discordCommandRegistry: DiscordCommandRegistry, capsFilter: DiscordCapsFilter, database: Database) {
 
@@ -37,11 +39,22 @@ class DiscordBotEventListener @Inject()(implicit configuration: Configuration, d
                     message.getAuthor
                 ))
             } else {
+                val coreSettings: Option[CoreSettings] = CoreSettings.get(message.getGuild.getID)
                 val filterSettings: Option[FilterSettings] = FilterSettings.get(message.getGuild.getID)
-                if (filterSettings.isDefined)
-                    capsFilter.filterMessage(message, filterSettings.get)
+                if (coreSettings.isDefined && message.getAuthor.getID != message.getGuild.getOwnerID) {
+                    if (coreSettings.get.moderatorRoleId.isDefined)
+                        if (!message.getAuthor.getRolesForGuild(message.getGuild).asScala.map(_.getID).contains(coreSettings.get.moderatorRoleId.get))
+                            filterMessage(message, filterSettings)
+                    else
+                        filterMessage(message, filterSettings)
+                }
                 //TODO figure out what mods want about warnings/bans/things
             }
+    }
+
+    def filterMessage(message: IMessage, filterSettings: Option[FilterSettings]): Unit = {
+        if (filterSettings.isDefined)
+            capsFilter.filterMessage(message, filterSettings.get)
     }
 
     @EventSubscriber
