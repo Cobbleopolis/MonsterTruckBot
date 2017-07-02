a866a24c0571bf769d3e325f705554173ac7f91d
Cobbleopolis <cobbleopolis@gmail.com>
2017-07-02 19:05:35 -0400

    The help command now lists the custom commands of a guild

--- modules/discord/app/discord/commands/DiscordHelpCommand.scala
+++ modules/discord/app/discord/commands/DiscordHelpCommand.scala
@@ -3,28 +3,32 @@ package discord.commands
 import javax.inject.Inject
 
 import com.cobble.bot.common.api.commands.HelpCommand
+import com.cobble.bot.common.models.CustomCommand
 import com.cobble.bot.common.ref.MtrConfigRef
 import com.google.inject.Provider
 import discord.DiscordCommandRegistry
 import discord.api.DiscordCommand
 import discord.event.CommandExecutionEvent
 import discord.util.DiscordMessageUtil
+import play.api.cache.CacheApi
+import play.api.db.Database
 import play.api.i18n.MessagesApi
 
-class DiscordHelpCommand @Inject()(val messageUtil: DiscordMessageUtil,
+class DiscordHelpCommand @Inject()(implicit val messageUtil: DiscordMessageUtil,
                                    messagesApi: MessagesApi,
                                    config: MtrConfigRef,
-                                   commandRegistry: Provider[DiscordCommandRegistry]
+                                   commandRegistry: Provider[DiscordCommandRegistry],
+                                   db: Database,
+                                   cacheApi: CacheApi
                                   ) extends DiscordCommand with HelpCommand {
 
     override def execute(implicit event: CommandExecutionEvent): Unit = {
-        var messageStr: String = ""
-        if (event.getArgs.isEmpty) {
-            messageStr += messagesApi("bot.help.general.prefix")
-            commandRegistry.get().commands.keys.foreach(msg =>
-                messageStr += messagesApi("bot.help.general.commandFormat", config.commandPrefix + msg, messagesApi(s"bot.help.descriptions.$msg"))
-            )
-        }
+        var messageStr: String = messagesApi("bot.help.general.prefix")
+        commandRegistry.get().commands.keys.foreach(msg =>
+            messageStr += messagesApi("bot.help.general.commandFormat", config.commandPrefix + msg, messagesApi(s"bot.help.descriptions.$msg"))
+        )
+        messageStr += messagesApi("bot.help.general.customCommandPrefix")
+        messageStr += CustomCommand.getByGuildId(config.guildId).map(_.commandName).mkString("`", s"`, `${config.commandPrefix}", "`")
         messageUtil.replyDM(messageStr)
     }
 
