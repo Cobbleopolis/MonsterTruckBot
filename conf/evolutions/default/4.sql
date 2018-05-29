c8560c5dc9de8a8d0bddbbf25af1a00e738d3ceb
Cobbleopolis <cobbleopolis@gmail.com>
2018-05-29 00:29:54 -0400

    Updated bit tracking schema in the database

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -5,11 +5,9 @@ CREATE TABLE bit_tracking_settings
 (
   guild_id                    BIGINT            NOT NULL,
   current_mode                INTEGER DEFAULT 0 NOT NULL,
-  nip_dip_template            TEXT DEFAULT '',
-  rbg_template                TEXT DEFAULT '',
-  jackshots_template          TEXT DEFAULT '',
-  push_up_template            TEXT DEFAULT '',
-  sing_it_or_slam_it_template TEXT DEFAULT '',
+  bit_game_message            TEXT DEFAULT '',
+  bits_message                TEXT DEFAULT '',
+  goal_message                TEXT DEFAULT '',
   CONSTRAINT bit_tracking_settings_bot_instances_guild_id_fk FOREIGN KEY (guild_id) REFERENCES bot_instances (guild_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
