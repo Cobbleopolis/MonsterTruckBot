f1db23762fdeec6edaf687d5c112345042046bb8
Cobbleopolis <cobbleopolis@gmail.com>
2018-05-29 17:56:02 -0400

    Fixed some issues in the database evolutions

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -3,14 +3,14 @@
 # --- !Ups
 CREATE TABLE bit_tracking_settings
 (
-  guild_id                    BIGINT            NOT NULL,
-  current_mode                INTEGER DEFAULT 0 NOT NULL,
-  bit_game_message            TEXT DEFAULT '',
-  bits_message                TEXT DEFAULT '',
-  goal_message                TEXT DEFAULT '',
+  guild_id         BIGINT            NOT NULL,
+  current_mode     INTEGER DEFAULT 0 NOT NULL,
+  bit_game_message TEXT DEFAULT '',
+  bits_message     TEXT DEFAULT '',
+  goal_message     TEXT DEFAULT '',
   CONSTRAINT bit_tracking_settings_bot_instances_guild_id_fk FOREIGN KEY (guild_id) REFERENCES bot_instances (guild_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 # -- !Downs
 
-DROP TABLE bit_tracking_settings
+DROP TABLE bit_tracking_settings;
